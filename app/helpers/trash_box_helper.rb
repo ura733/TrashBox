b47fcb0a15cd2498a80853555f8108d00170e387
kosuke <depths.ptk@gmail.com>
2018-12-06 23:55:06 +0900

    add header footer

---
+++ app/helpers/trash_box_helper.rb
@@ -0,0 +1,2 @@
+module TrashBoxHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'trash_box#home'
+  
+  get '/about', to: 'trash_box#about'
+  get '/help', to: 'trash_box#help'
+  get '/contact', to: 'trash_box#contact'
 end
