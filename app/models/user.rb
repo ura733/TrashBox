414182566aff92c27334f7ed747cf8332a2280f7
kosuke <depths.ptk@gmail.com>
2018-12-11 16:23:46 +0900

    add user model spec validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255},
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  validates :password, length: { minimum: 6 }
 
   has_many :records
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user, name: 'userA', email: 'usera@example.com', password: 'password', password_confirmation: 'password')
+  end
+
+  context 'user validation' do
+    it '値が入っている場合' do
+      expect(@user).to be_valid
+    end
+    it 'nameがない空白' do
+      @user.name = ''
+      expect(@user).to be_invalid
+    end
+    it 'emailが空白' do
+      @user.email = ''
+      expect(@user).to be_invalid
+    end
+  end
+
+  context 'email validation' do
+    it '文字数が255字以上' do
+      @user.email = 'a' * 244 + '@example.com'
+      expect(@user).to be_invalid
+    end
+
+    it 'メールフォーマットに合っているか' do
+      addresses = %w[user@example,com user_at_foo.org user.name@example.
+                            foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |address|
+        @user.email = address
+        expect(@user).to be_invalid
+      end
+    end
+
+    it 'メールアドレスが一意になっているか' do
+      duplicate_user = @user.dup
+      @user.save
+      expect(duplicate_user).to be_invalid
+    end
+  end
+
+  context 'name validation' do
+    it '文字数が50字以上' do
+      @user.name = 'a' * 51
+      expect(@user).to be_invalid
+    end
+  end
+
+  context 'password validation' do
+  it '文字数が6字未満' do
+    @user.password = 'aaaaa'
+    @user.password_confirmation = @user.password
+    expect(@user).to be_invalid
+  end
+  end
 end
